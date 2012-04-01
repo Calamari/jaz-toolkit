94f10f125d245051f803266aaa2e56171014ac35
Georg Tavonius <georg.tavonius@xing.com>
2012-04-01 19:56:02 +0200

    Added Date.fromMySQLString method

--- src/Date.js
+++ src/Date.js
@@ -4,5 +4,9 @@
 module.exports = {
 	toMySQLString: function(date) {
 		return date.toISOString().replace('T', ' ').split('.')[0];
+	},
+	fromMySQLString: function(dateString) {
+		var parts = dateString.replace(/-/g, ' ').replace(/:/g, ' ').split(' ');
+		return new Date(Date.UTC(parts[0], parts[1]-1, parts[2], parts[3], parts[4], parts[5]));
 	}
 };
--- test/Date.js
+++ test/Date.js
@@ -14,6 +14,14 @@ vows.describe('Date')
           mysqlNow = JazDate.toMySQLString(now);
 
       assert.equal(mysqlNow, now.toISOString().replace('T', ' ').split('.')[0]);
+    },
+
+    'fromMySQLString': function(JazDate) {
+      var now      = new Date(2011, 10, 20, 2, 2, 3),
+          mysqlNow = JazDate.toMySQLString(now);
+
+      assert.equal(JazDate.fromMySQLString(mysqlNow).constructor, Date);
+      assert.equal(JazDate.fromMySQLString(mysqlNow).getTimezoneOffset(), now.getTimezoneOffset());
     }
   }
 }).export(module);
